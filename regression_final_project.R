b25ca773a7b6f9c4b7cdcc268118a7344cb6cc98
Robert Gorman <rbtgorman@gmail.com>
2025-02-26 11:30:54 -0500

    update folder

---
+++ regression_final_project.R
@@ -0,0 +1,38 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(ggcorrplot)
+
+
+data <- read_csv("supermarket_sales - Sheet1.csv")
+
+sum(is.na(data))
+summary(data)
+
+ggcorrplot(cor_matrix, lab = TRUE)
+
+
+# Converting time into hours so it is accessible
+data <- data %>%
+  mutate(Hours = hour(hms::as_hms(Time)) + minute(hms::as_hms(Time)) / 60)
+
+
+model <- lm(Total ~ Quantity + `Unit price` + Rating + Hours, data = data)
+summary(model)
+
+### Hours relationship with the 'Total' value is not explained so I will create
+### a graph to show AVG sales by hour of day. I wi
+
+time_analysis <- data %>%
+  group_by(Hours = floor(Hours)) %>%
+  summarize(Average_Total = mean(Total))
+
+ggplot(time_analysis, aes(x = Hours, y = Average_Total)) +
+  geom_line() +
+  labs(title = "AVG sales by Hour of Day", x = "Hour of Day", y = "Average Total Sales")
+
+# Rating analysis
+rating_analysis <- lm(Total ~ Rating, data = data)
+summary(rating_analysis)
+
+
